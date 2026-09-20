6b4f681edeaf11d9d9135c5eb0d82bbad47beeee
AndyMarlonium <103562542+AndyMarlonium@users.noreply.github.com>
2026-09-20 19:12:00 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "vintana-v7";
+const CACHE = "vintana-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE)
